4d918fa8b9bba311d1a05b152a88404012272700
Sally Bean <beansamuel12345@hotmail.com>
2024-12-22 20:00:12 +0000

    recommendation algorithm changes

---
+++ NEA/user.py
@@ -0,0 +1,35 @@
+class User:
+    def __init__(self):
+        return False
+
+    def login(self):
+        pass
+
+    def get_watchlists(self):
+        pass
+
+    def get_info(self):
+        pass
+
+    def get_recently_watched_films(self):
+        pass
+
+    def get_rating(self, film):
+        pass
+
+    def get_correlation_coefficient(self, friend):
+        pass
+
+
+class ClientUser(User):
+    def log_film(self):
+        pass
+
+    def add_friend(self):
+        pass
+
+    def accept_friend(self):
+        pass
+
+    def set_info(self):
+        pass
